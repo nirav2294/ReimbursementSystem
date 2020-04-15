5732acdcc7ac292d3117773d9805ae2fbcd30363
Nirav Patel <niravpatel@Niravs-MacBook-Pro.local>
2020-04-15 17:10:51 -0400

    Created Daos and utils

---
+++ ExpenseReimbursementSystem/src/main/java/dev/patel/daos/EmployeeDAO.java
@@ -0,0 +1,14 @@
+package dev.patel.daos;
+
+import java.util.List;
+
+import dev.patel.entities.Employee;
+
+public interface EmployeeDAO {
+	
+	List<Employee> getAllEmployees();
+	Employee getEmployeeById(int id);
+	Employee getEmployeeByUsername(String username);
+	Employee validateEmployee(String username, String password);
+
+}
---
+++ ExpenseReimbursementSystem/src/main/java/dev/patel/daos/ReimbursementDAOImpl.java
@@ -0,0 +1,59 @@
+package dev.patel.daos;
+
+import java.util.List;
+
+import dev.patel.entities.Reimbursement;
+
+public class ReimbursementDAOImpl implements ReimbursementDAO{
+
+	@Override
+	public Reimbursement createReimbursement(Reimbursement reimbursement) {
+		return null;
+	}
+
+	@Override
+	public List<Reimbursement> getAllReimbursements() {
+		return null;
+	}
+
+	@Override
+	public Reimbursement getReimbursementsById(int id) {
+		return null;
+	}
+
+	@Override
+	public List<Reimbursement> getPendingReimbursements() {
+		return null;
+	}
+
+	@Override
+	public List<Reimbursement> getApprovedReimbursements() {
+		return null;
+	}
+
+	@Override
+	public List<Reimbursement> getReimbursementByEmployeeId(int employeeId) {
+		return null;
+	}
+
+	@Override
+	public List<Reimbursement> getPendingRequestsByEmployeeId(int employeeId) {
+		return null;
+	}
+
+	@Override
+	public List<Reimbursement> getApprovedRequestsByEmployeeId(int employeeId) {
+		return null;
+	}
+
+	@Override
+	public List<Reimbursement> getReimbursementByManagerId(int mId) {
+		return null;
+	}
+
+	@Override
+	public Reimbursement updateReimbursement(Reimbursement reimbursement) {
+		return null;
+	}
+
+}
