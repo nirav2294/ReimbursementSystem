5db92cd48dc3c0b38cb716a33609f352c203266d
nirav2294 <44714261+nirav2294@users.noreply.github.com>
2020-04-27 02:15:32 -0400

    Update index.js

--- ExpenseReimbursementSystem/src/main/webapp/JS/index.js
+++ ExpenseReimbursementSystem/src/main/webapp/JS/index.js
@@ -20,16 +20,16 @@ async function employeeLogin() {
         body: JSON.stringify(login)
     }
 
-    let httpResponse = await fetch("http://localhost:8080/ExpenseReimbursementSystem/api/LoginPage?username=" + login.userName, settings);
+    let httpResponse = await fetch("http://ec2-3-17-161-23.us-east-2.compute.amazonaws.com:8080/ExpenseReimbursementSystem/api/LoginPage?username=" + login.userName, settings);
     let user = await httpResponse.json();
     console.log(user)
 
     if (user.employeeId != 0) {
-        window.location.href = "http://localhost:8080/ExpenseReimbursementSystem/EmployeeHome.html";
+        window.location.href = "http://ec2-3-17-161-23.us-east-2.compute.amazonaws.com:8080/ExpenseReimbursementSystem/EmployeeHome.html";
     }
     else {
         document.getElementById("alertmessage").innerHTML = `<i class="material-icons" style="font-size:16px" >error</i>` + " Incorrect username or password";
-        //window.location.href = "http://localhost:8080/ExpenseReimbursementSystem/index.html";
+        //window.location.href = "http://ec2-3-17-161-23.us-east-2.compute.amazonaws.com:8080/ExpenseReimbursementSystem/index.html";
     }
 }
 
@@ -52,13 +52,13 @@ async function managerLogin() {
         body: JSON.stringify(login)
     }
 
-    let httpResponse = await fetch("http://localhost:8080/ExpenseReimbursementSystem/api/ManagerLoginPage?username=" + login.userName, settings);
+    let httpResponse = await fetch("http://ec2-3-17-161-23.us-east-2.compute.amazonaws.com:8080/ExpenseReimbursementSystem/api/ManagerLoginPage?username=" + login.userName, settings);
     let user = await httpResponse.json();
     console.log(user.managerId)
     if (user.managerId != 0) {
-        window.location.href = "http://localhost:8080/ExpenseReimbursementSystem/ManagerHome.html";
+        window.location.href = "http://ec2-3-17-161-23.us-east-2.compute.amazonaws.com:8080/ExpenseReimbursementSystem/ManagerHome.html";
     }
     else {
-        window.location.href = "http://localhost:8080/ExpenseReimbursementSystem/index.html";
+        window.location.href = "http://ec2-3-17-161-23.us-east-2.compute.amazonaws.com:8080/ExpenseReimbursementSystem/index.html";
     }
 }
