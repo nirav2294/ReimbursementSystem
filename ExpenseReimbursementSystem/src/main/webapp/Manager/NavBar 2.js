7aa8aad04dcebc0a226d982e3424075ab5abb6a3
Nirav Patel <niravpatel@Niravs-MacBook-Pro.local>
2020-04-27 02:08:07 -0400

    Final Project with Stats Page

--- ExpenseReimbursementSystem/src/main/webapp/Manager/NavBar 2.js
+++
@@ -1,8 +0,0 @@
-
-
-
-async function logout() {
-    let httpResponse = await fetch("http://localhost:8080/ExpenseReimbursementSystem/api/Logout");
-    let logout = await httpResponse.json();
-    console.log(user);
-}
