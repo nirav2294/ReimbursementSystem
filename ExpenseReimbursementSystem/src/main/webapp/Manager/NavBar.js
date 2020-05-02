1e6c047601d0233f4c01da5aa07de5f6d1d7f1cb
nirav2294 <44714261+nirav2294@users.noreply.github.com>
2020-05-02 14:51:11 -0400

    Update NavBar.js

--- ExpenseReimbursementSystem/src/main/webapp/Manager/NavBar.js
+++ ExpenseReimbursementSystem/src/main/webapp/Manager/NavBar.js
@@ -2,7 +2,7 @@
 
 
 async function logout() {
-    let httpResponse = await fetch("http://ec2-3-17-161-23.us-east-2.compute.amazonaws.com:8080/ExpenseReimbursementSystem/api/Logout");
+    let httpResponse = await fetch("http://ec2-3-133-103-178.us-east-2.compute.amazonaws.com:8080/ExpenseReimbursementSystem/api/Logout");
     let logout = await httpResponse.json();
     console.log(user);
 }
